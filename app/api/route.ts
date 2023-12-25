d0a381be23bd494444520e64cd396d2db81452a9
WooFoo <sujanstha3220@gmail.com>
2023-12-25 19:12:36 +0545

    fix : NextApiRequrest error fixed

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,11 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export interface responseProps {
 	data: string;
 }
 
 export async function GET(
-	request: NextApiRequest
+	request: NextRequest
 ): Promise<NextResponse<responseProps>> {
 	const data = { data: "data" };
 	return NextResponse.json(data);

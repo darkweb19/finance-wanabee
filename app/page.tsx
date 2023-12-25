08002703583ff7003c669b8fc50f48032210ae72
WooFoo <sujanstha3220@gmail.com>
2023-12-25 19:18:39 +0545

    fix : removed api fetching in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-async function getUser() {
-	try {
-		const data = await fetch("/api/user");
-		const finalData = await data.json();
-		console.log(finalData);
-	} catch (err) {
-		console.log("Cannot fetch req", err);
-	}
-}
-
 export default function Home() {
-	getUser();
 	return (
 		<main className="text-center h-screen flex justify-center items-center gap-2">
 			Hello World

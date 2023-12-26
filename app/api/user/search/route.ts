ddec120a87705091f93dae01af6a5f1592e2ec0f
WooFoo <sujanstha3220@gmail.com>
2023-12-26 20:04:08 +0545

    feat  : opting new data

--- app/api/user/search/route.ts
+++ app/api/user/search/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/prisma/Prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
 	try {
 		const searchName = req.nextUrl.searchParams.get("name");

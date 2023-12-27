fcc2fa1d0b45d95f222f33cc02970e089dcb5264
WooFoo <sujanstha3220@gmail.com>
2023-12-27 19:30:54 +0545

    feat : swr package is added for client side revalidation

--- app/user/get/page.tsx
+++ app/user/get/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Link from "next/link";
 import UserList from "./UserList";
+import useSWR from "swr";
 
-async function getUsers() {
+const getUsers = async () => {
 	const users = await fetch(
 		"https://finance-wanabee.vercel.app/api/user/get",
 		{
@@ -13,16 +15,15 @@ async function getUsers() {
 	);
 	const user = await users.json();
 	return user;
-}
+};
 
-export default async function User() {
-	const user = await getUsers();
+export default function User() {
+	const { data, error } = useSWR("key", getUsers);
 
 	return (
 		<main className="h-screen flex flex-col gap-10 justify-center items-center">
 			<div>lists</div>
-
-			<UserList user={user} />
+			{data && <UserList user={data} />}
 			<Link className="border p-2" href="/">
 				back
 			</Link>

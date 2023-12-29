3c347087c9a324e33c485078676bf3d5837b84e0
WooFoo <sujanstha3220@gmail.com>
2023-12-29 15:12:35 +0545

    feat : revalidated path after deleting

--- app/user/get/util.ts
+++ app/user/get/util.ts
@@ -1,9 +1,11 @@
 "use server";
 import prisma from "@/prisma/Prisma";
+import { revalidatePath } from "next/cache";
 
 export async function deleteUser(id: string) {
 	try {
 		await prisma.user.delete({ where: { id: id } });
+		revalidatePath("/user/get");
 		return { success: true, message: "User Deleted Successfully" };
 	} catch (err: any) {
 		console.log("error while deleting user", err.message);

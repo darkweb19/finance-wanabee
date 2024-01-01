0f2ea59dceaa0a73ddc58f030abb0e1f8acd2f93
WooFoo <sujanstha3220@gmail.com>
2024-01-01 14:10:19 +0545

    feat : for development purposes , data migration is added for dummy users

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -1,10 +1,40 @@
 import prisma from "@/prisma/Prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+async function dataMigration() {
+	const fetchData = async () => {
+		const response = await fetch(
+			"https://dummyjson.com/users?limit=50&select=firstName,email,age,gender,username,weight"
+		);
+		const data = await response.json();
+		return data;
+	};
+
+	const userData = await fetchData();
+	console.log(userData.users[0]);
+	const insertData = async () => {
+		for (const user of userData.users) {
+			await prisma.user.create({
+				data: {
+					name: user.firstName,
+					email: user.email,
+					balance: user.id,
+					username: user.username,
+					age: user.age,
+					gender: user.gender,
+					weight: user.weight,
+				},
+			});
+		}
+	};
+	insertData();
+	console.log("data migrated");
+}
+
 export async function POST(req: NextRequest) {
 	try {
 		const { name, email, balance } = await req.json();
-
+		// dataMigration();
 		if (!name || !email) {
 			throw new Error("Cannot add empty in db");
 		}

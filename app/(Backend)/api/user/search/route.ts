8efa7a89afe9c7a847e37cb90d7eb82a542546c3
WooFoo <sujanstha3220@gmail.com>
2024-01-01 15:37:12 +0545

    feat : refac and added search params

--- app/(Backend)/api/user/search/route.ts
+++ app/(Backend)/api/user/search/route.ts
@@ -6,15 +6,21 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
 	try {
 		const searchName = req.nextUrl.searchParams.get("name");
+
 		const user = await prisma.user.findMany({
-			where: { name: searchName as string },
+			where: {
+				name: { contains: searchName as string, mode: "insensitive" },
+			},
 		});
 
 		if (user.length == 0) {
-			return NextResponse.json({ null: "nothing is found" });
+			return NextResponse.json({
+				success: false,
+				null: "nothing is found",
+			});
 		}
 
-		return NextResponse.json(user);
+		return NextResponse.json({ success: true, user });
 	} catch (err) {
 		console.log("getting user failed");
 		return NextResponse.json({
---
+++ app/(frontend)/user/get/search/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { User } from "@prisma/client";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function SearchUser() {
+	const [name, setName] = useState("");
+	const [user, setUser] = useState<User>(Object);
+
+	async function handleSearch() {
+		const searchUser = await fetch(
+			`https://finance-wanabee.verce.app/api/user/search?name=${name}`
+		);
+
+		const resUser = await searchUser.json();
+
+		if (!resUser.success) {
+			toast.error("Cannnot found the user in the db");
+		} else {
+			setUser(resUser.user[0]);
+			toast.success("user found");
+		}
+	}
+
+	return (
+		<div className=" h-screen flex gap-3 flex-col items-center justify-center">
+			<div className=" flex flex-col w-[50%] gap-3 items-center">
+				<input
+					className="text-black p-1 rounded-lg"
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<button
+					className="border rounded-lg p-2"
+					onClick={() => handleSearch()}
+				>
+					Search
+				</button>
+			</div>
+
+			<div className="border flex flex-col rounded-md p-3 items-center">
+				<ul className="flex flex-col gap-2">
+					{user != undefined && user.email ? (
+						<>
+							<li>Name : {user.name}</li>
+							<li>Age : {user.age}</li>
+							<li>Email : {user.email}</li>
+							<li>Gender : {user.gender}</li>
+							<li>UserName : {user.username}</li>
+							<li>Weight : {user.weight}</li>
+						</>
+					) : (
+						"no data"
+					)}{" "}
+				</ul>
+			</div>
+		</div>
+	);
+}

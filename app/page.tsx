24cef5afd399224417d403ad821a70deeaa4d690
WooFoo <sujanstha3220@gmail.com>
2024-01-07 11:45:17 +0545

    fix : minor bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { lilitaFont } from "./layout";
 import NavigationLinks from "./Navigation";
 
 export default function Home() {
 	return (
 		<main className="text-center p-3 h-dvh sm:h-screen flex flex-col justify-center items-center gap-10">
-			<span
-				className={`${lilitaFont.className} text-3xl sm:text-4xl text-slate-600`}
-			>
+			<span className={` text-3xl sm:text-4xl text-slate-600`}>
 				Finance Wanabee
 			</span>
 			<NavigationLinks />

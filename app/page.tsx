4da305a9817a384076521abc9d3bfae2a135bb00
WooFoo <sujanstha3220@gmail.com>
2024-01-07 11:56:44 +0545

    feat : lilitaOne font is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import NavigationLinks from "./Navigation";
+import { Lilita_One } from "next/font/google";
+
+const lilitaFont = Lilita_One({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<main className="text-center p-3 h-dvh sm:h-screen flex flex-col justify-center items-center gap-10">
-			<span className={` text-3xl sm:text-4xl text-slate-600`}>
+			<span
+				className={`${lilitaFont.className} text-3xl sm:text-4xl text-slate-600`}
+			>
 				Finance Wanabee
 			</span>
 			<NavigationLinks />

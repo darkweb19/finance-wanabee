b0d3225ffc7be640be52b89ad126a91c20966ffe
WooFoo <sujanstha3220@gmail.com>
2024-01-23 18:06:31 +0545

    feat : email provider

--- app/(Backend)/api/auth/[...nextauth]/route.ts
+++ app/(Backend)/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,27 @@
 import prisma from "@/prisma/Prisma";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 const authOptions: NextAuthOptions = {
 	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		EmailProvider({
+			server: {
+				host: process.env.EMAIL_SERVER_HOST,
+				port: process.env.EMAIL_SERVER_PORT,
+				auth: {
+					user: process.env.EMAIL_SERVER_USER,
+					pass: process.env.EMAIL_SERVER_PASSWORD,
+				},
+			},
+			from: process.env.EMAIL_FROM,
 		}),
 	],
 	callbacks: {
